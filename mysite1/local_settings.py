1da415c3aefd4dcff86cbe0e1eae9a77d88eeaf9
小室誠 <komuromakoto@komuromakotonoMacBook-Pro.local>
2022-09-08 00:18:15 +0900

    deploy heroku

--- mysite1/local_settings.py
+++ mysite1/local_settings.py
@@ -1,14 +1,12 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-SECRET_KEY = 'django-insecure-i#^60kom&v5akh8u%nacki7=wl0uf=#^2t*-_-fz0t!ly=g29n'
+SECRET_KEY = 'settings.pyからコピーしたSECRET_KEYをここに貼り付ける'
 
 #settings.pyからそのままコピー
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'TEST': {
-            'NAME': 'mytestdb',
-        }}
+ 'default': {
+ 'ENGINE': 'django.db.backends.sqlite3',
+ 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ }
 }
 DEBUG = True
